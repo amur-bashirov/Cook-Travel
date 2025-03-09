1808f8f2408aed50379c9436562a7adc4a71d96c
Amur Bashirov <bashirov@byu.edu>
2025-03-08 18:41:45 -0700

    edited unauthenticated.jsx

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import "../app.css";
+import "./login.css";
 
 import Button from 'react-bootstrap/Button';
 import { MessageDialog } from './messageDialog';
@@ -20,24 +22,31 @@ export function Unauthenticated(props) {
 
   return (
     <>
-      <div>
+    <main className="container-fluid text-center">
+    <div>
+      <h1>Welcome to <em>Eat&Travel</em></h1>
+      <p>Please log in to access all features.</p>
+      <form method="get" action="play.html">
         <div className='input-group mb-3'>
-          <span className='input-group-text'>@</span>
-          <input className='form-control' type='text' value={userName} onChange={(e) => setUserName(e.target.value)} placeholder='your@email.com' />
+        <span className='input-group-text'>@</span>
+        <input className='form-control' type='text' value={userName} onChange={(e) => setUserName(e.target.value)} placeholder='your@email.com' />
         </div>
         <div className='input-group mb-3'>
-          <span className='input-group-text'>🔒</span>
-          <input className='form-control' type='password' onChange={(e) => setPassword(e.target.value)} placeholder='password' />
+        <span className='input-group-text'>🔒</span>
+        <input className='form-control' type='password' onChange={(e) => setPassword(e.target.value)} placeholder='password' />
         </div>
         <Button variant='primary' onClick={() => loginUser()} disabled={!userName || !password}>
-          Login
+        Login
         </Button>
         <Button variant='secondary' onClick={() => createUser()} disabled={!userName || !password}>
-          Create
+        Create
         </Button>
-      </div>
-
+        <p> Don't have an account? Sign up now to create and share your own recipes, get exclusive cooking tips, and explore travel advice from around the world!</p>
+      </form>
       <MessageDialog message={displayError} onHide={() => setDisplayError(null)} />
+    </div>
+  </main> 
     </>
+
   );
 }
