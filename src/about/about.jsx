ca223ee448d09ac123c414b4932cc1ac6fdf1880
Amur Bashirov <bashirov@byu.edu>
2025-03-31 19:37:47 -0600

    successfuly downloads quote from the third party

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -3,6 +3,24 @@ import "../app.css" ;
 import "./about.css" ;
 
 export function About() {
+  const [imageUrl, setImageUrl] = React.useState('data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=');
+  const [quote, setQuote] = React.useState('Loading...');
+  const [quoteAuthor, setQuoteAuthor] = React.useState('unknown');
+
+
+  React.useEffect(() => {
+
+    fetch('https://quote.cs260.click')
+      .then((response) => response.json())
+      .then((data) => {
+        setQuote(data.quote);
+        setQuoteAuthor(data.author);
+      })
+      .catch();
+  }, []);
+
+
+
   return (
     <main className="container-fluid text-center">
       <div>Have you ever wondered how to cook local cuisine while traveling in a foreign country,
@@ -13,6 +31,10 @@ export function About() {
          It offers access to millions of recipes and step-by-step instructions for the most popular dishes from around the world.
          </p>
         </div>
+      
+        
+        <p className='quote'>{quote}</p>
+        <p className='author'>{quoteAuthor}</p>
     </main>
   );
 }
