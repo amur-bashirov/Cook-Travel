2b2a16e15e9939680454492e042bf84bfc1c1c9c
Amur Bashirov <bashirov@byu.edu>
2025-04-07 22:31:23 -0600

    modified database.js

--- service/database.js
+++ service/database.js
@@ -3,9 +3,9 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('simon');
+const db = client.db('Cook-Travel');
 const userCollection = db.collection('user');
-const scoreCollection = db.collection('score');
+const scoreCollection = db.collection('posts');
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
@@ -34,25 +34,16 @@ async function updateUser(user) {
   await userCollection.updateOne({ email: user.email }, { $set: user });
 }
 
-async function addScore(score) {
-  return scoreCollection.insertOne(score);
+async function addPost(post) {
+  return scoreCollection.insertOne(post);
 }
 
-function getHighScores() {
-  const query = { score: { $gt: 0, $lt: 900 } };
-  const options = {
-    sort: { score: -1 },
-    limit: 10,
-  };
-  const cursor = scoreCollection.find(query, options);
-  return cursor.toArray();
-}
+
 
 module.exports = {
   getUser,
   getUserByToken,
   addUser,
   updateUser,
-  addScore,
-  getHighScores,
+  addPost
 };
