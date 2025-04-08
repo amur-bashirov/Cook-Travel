697d0614ce10c5caf1b59ab5b5edba15391f7953
Amur Bashirov <bashirov@byu.edu>
2025-04-08 15:50:38 -0600

    modified

--- service/postProxy.js
+++ service/postProxy.js
@@ -1,36 +1,68 @@
 const { WebSocketServer } = require('ws');
 
 function postProxy(httpServer) {
-  // Create a websocket object
   const socketServer = new WebSocketServer({ server: httpServer });
+
+  // Store connected clients with their associated userName
+  const clients = new Map();
 
   socketServer.on('connection', (socket) => {
     socket.isAlive = true;
 
-    // Forward messages to everyone except the sender
-    socket.on('message', function message(data) {
-      socketServer.clients.forEach((client) => {
-        if (client !== socket && client.readyState === WebSocket.OPEN) {
-          client.send(data);
+    // Expect the client to send their userName upon connection
+    socket.on('message', (data) => {
+      try {
+        const message = JSON.parse(data);
+
+        // If the message contains a userName, associate it with the socket
+        if (message.type === 'register' && message.userName) {
+          clients.set(message.userName, socket);
+          socket.userName = message.userName;
         }
-      });
+      } catch (err) {
+        console.error('Error parsing message:', err);
+      }
     });
 
     // Respond to pong messages by marking the connection alive
     socket.on('pong', () => {
       socket.isAlive = true;
     });
+
+    // Handle socket closure
+    socket.on('close', () => {
+      if (socket.userName) {
+        clients.delete(socket.userName);
+      }
+    });
   });
 
   // Periodically send out a ping message to make sure clients are alive
   setInterval(() => {
-    socketServer.clients.forEach(function each(client) {
-      if (client.isAlive === false) return client.terminate();
+    socketServer.clients.forEach((client) => {
+      if (!client.isAlive) {
+        if (client.userName) {
+          clients.delete(client.userName);
+        }
+        return client.terminate();
+      }
 
       client.isAlive = false;
       client.ping();
     });
   }, 10000);
+
+  // Function to send a message to a specific user
+  function sendMessageToUser(userName, message) {
+    const client = clients.get(userName);
+    if (client && client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(message));
+    }
+  }
+
+  return {
+    sendMessageToUser,
+  };
 }
 
-module.exports = { postProxy: postProxy };
+module.exports = { postProxy };
